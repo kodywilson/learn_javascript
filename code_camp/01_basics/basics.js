015013a3866d3e24e8f4bc4a519701411917cea5
Kody Wilson <kodywilson@gmail.com>
2020-01-13 16:46:02 -0800

    variables

--- code_camp/01_basics/basics.js
+++ code_camp/01_basics/basics.js
@@ -1,8 +1,24 @@
 console.log(3 + 5);
-
-var number = 5;// This is a comment
+// Section on Comments
+var number = 5; // This is a comment
 
 /* Multi
  line
  commment
  */
+
+// Section on variables
+var myName = "Rocky";
+console.log(myName);
+myName = "Road";
+console.log(myName);
+
+let testVar = "this is only a test";
+console.log(testVar);
+
+const pie = 3.14;
+console.log(pie);
+
+var a;
+var b = 2;
+console.log("a = " + a + "and b = " + b);
